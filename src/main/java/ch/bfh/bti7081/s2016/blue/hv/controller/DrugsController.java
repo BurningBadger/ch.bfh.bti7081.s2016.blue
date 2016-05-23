1c05b151e27dc7defd73ed5835be54a4e41e3152
Nicolas Schmid <nicolasschmid@192.168.1.5>
2016-05-23 15:52:31 +0200

    added BaseContainer with all CRUD operations.

--- src/main/java/ch/bfh/bti7081/s2016/blue/hv/controller/DrugsController.java
+++ src/main/java/ch/bfh/bti7081/s2016/blue/hv/controller/DrugsController.java
@@ -2,21 +2,15 @@ package ch.bfh.bti7081.s2016.blue.hv.controller;
 
 import java.util.logging.Logger;
 
-import com.vaadin.addon.jpacontainer.JPAContainer;
-import com.vaadin.addon.jpacontainer.JPAContainerFactory;
-
 import ch.bfh.bti7081.s2016.blue.hv.model.Drug;
-import ch.bfh.bti7081.s2016.blue.hv.util.Constants;
 
-public class DrugsController extends BaseController {
+public class DrugsController extends BaseController<Drug, Long> {
 
-	private static final long serialVersionUID = -3477963697182614276L;
+    private static final long serialVersionUID = -3477963697182614276L;
 
-	private final static Logger LOGGER = Logger.getLogger(DrugsController.class.getName());
+    private final static Logger LOGGER = Logger.getLogger(DrugsController.class.getName());
 
-	private final JPAContainer<Drug> drugs;
-
-	public DrugsController() {
-		drugs = JPAContainerFactory.make(Drug.class, Constants.PERSISTENCE_UNIT);
-	}
+    public DrugsController() {
+	super(Drug.class);
+    }
 }
--- src/main/java/ch/bfh/bti7081/s2016/blue/hv/controller/VisitsController.java
+++ src/main/java/ch/bfh/bti7081/s2016/blue/hv/controller/VisitsController.java
@@ -2,23 +2,16 @@ package ch.bfh.bti7081.s2016.blue.hv.controller;
 
 import java.util.logging.Logger;
 
-import com.vaadin.addon.jpacontainer.JPAContainer;
-import com.vaadin.addon.jpacontainer.JPAContainerFactory;
-
 import ch.bfh.bti7081.s2016.blue.hv.model.Visit;
-import ch.bfh.bti7081.s2016.blue.hv.util.Constants;
 import ch.bfh.bti7081.s2016.blue.hv.view.VisitsView;
 
-public class VisitsController extends BaseController {
+public class VisitsController extends BaseController<Visit, Long> {
 
     private static final long serialVersionUID = -5573378810271266249L;
 
     private final static Logger LOGGER = Logger.getLogger(VisitsController.class.getName());
 
-    private JPAContainer<Visit> visits;
-
     public VisitsController(VisitsView view) {
-	visits = JPAContainerFactory.make(Visit.class, Constants.PERSISTENCE_UNIT);
-	view.setVisits(visits);
+	super(Visit.class);
     }
 }
