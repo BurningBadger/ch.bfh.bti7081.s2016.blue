43a759880b84f998b5c45b117fdcc0b362d00372
ugorcak <ugorcak@gmx.ch>
2016-05-29 00:35:23 +0200

    PatientListView, PatientView: connected DB, add/edit Patient

--- src/main/java/ch/bfh/bti7081/s2016/blue/hv/view/PatientListView.java
+++ src/main/java/ch/bfh/bti7081/s2016/blue/hv/view/PatientListView.java
@@ -1,109 +1,161 @@
 package ch.bfh.bti7081.s2016.blue.hv.view;
 
-import com.vaadin.addon.jpacontainer.JPAContainer;
-import com.vaadin.addon.jpacontainer.JPAContainerFactory;
-import com.vaadin.data.Container;
-import com.vaadin.data.util.filter.Compare;
+import ch.bfh.bti7081.s2016.blue.hv.model.PatientModel;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.*;
 
 import ch.bfh.bti7081.s2016.blue.hv.entities.Patient;
-import ch.bfh.bti7081.s2016.blue.hv.test.EntityService;
 
-/**
- * Created by uck1 on 22.05.2016.
- */
 public class PatientListView extends VerticalLayout implements View {
 
     private static final String NAME = "Patients"; // variable for HealthVisUI
 
-    private Button butMenu = new Button("Menu");
-    private Button butCall = new Button("Call");
-    private Button butGestern = new Button("Gestern");
-    private Button butHeute = new Button("Heute");
-    private Button butMorgen = new Button("Morgen");
-    private Button butTermine = new Button("Termine");
-    private Button butRoute = new Button("Route");
-    private Button butKalender = new Button("Kalender");
-
     public PatientListView() {
-//	setSizeFull();
-	buildLayout();
-    }
-
-    private void buildLayout() {
-	EntityService es = new EntityService();
-	es.create();
 
 	Panel panel = new Panel();
 	panel.setSizeFull();
 
 	// 1st row
 	HorizontalLayout firstLay = new HorizontalLayout();
+	Button butMenu = new Button("Menu");
 	firstLay.addComponent(butMenu);
 	firstLay.setComponentAlignment(butMenu, Alignment.MIDDLE_LEFT);
+	Button butCall = new Button("Call");
 	firstLay.addComponent(butCall);
 	firstLay.setComponentAlignment(butCall, Alignment.MIDDLE_RIGHT);
 	firstLay.setMargin(true);
-	butMenu.addStyleName("btntestclass");
-	butCall.addStyleName("btntestclass");
+//	butMenu.addStyleName("btntestclass");
+//	butCall.addStyleName("btntestclass");
 	firstLay.setWidth("100%");
 	this.addComponent(firstLay);
 
 	// 2nd row
 	HorizontalLayout horizontalTage = new HorizontalLayout();
+	Button butGestern = new Button("Gestern");
 	horizontalTage.addComponent(butGestern);
 	horizontalTage.setComponentAlignment(butGestern, Alignment.MIDDLE_LEFT);
+	Button butHeute = new Button("Heute");
 	horizontalTage.addComponent(butHeute);
 	horizontalTage.setComponentAlignment(butHeute, Alignment.MIDDLE_CENTER);
+	Button butMorgen = new Button("Morgen");
 	horizontalTage.addComponent(butMorgen);
 	horizontalTage.setComponentAlignment(butMorgen, Alignment.MIDDLE_RIGHT);
 	horizontalTage.setMargin(true);
-	butGestern.addStyleName("btntestclass");
-	butHeute.addStyleName("btntestclass");
-	butMorgen.addStyleName("btntestclass");
+//	butGestern.addStyleName("btntestclass");
+//	butHeute.addStyleName("btntestclass");
+//	butMorgen.addStyleName("btntestclass");
 	horizontalTage.setWidth("100%");
 	this.addComponent(horizontalTage);
 
 	// table row
-	JPAContainer<Patient> patients = JPAContainerFactory.make(Patient.class, "healthVisApp");
+	final Table table = new Table();
+	table.addStyleName("components-inside");
 
-	Table personTable = new Table();
-	personTable.setContainerDataSource(patients);
-	personTable.setSelectable(true);
-	personTable.setImmediate(true);
+	// define the columns
+	table.addContainerProperty("first name", Label.class, null);
+	table.addContainerProperty("last name", Label.class,  null);
+	table.addContainerProperty("birthday", Label.class, null);
+	table.addContainerProperty("", Button.class, null);
 
-	final TextField name = new TextField();
-	name.setCaption("Type your name here:");
+	// connect with the DB and add data into the table
+	PatientModel patientModel = new PatientModel();
 
-	Button button = new Button("Click Me");
-	button.addClickListener(e -> {
-	    Container.Filter filter = new Compare.Equal("firstname", name.getValue());
-	    patients.addContainerFilter(filter);
-	    patients.addNestedContainerProperty("drugs.*");
-	    this.addComponent(new Table("The patients drugs", patients));
+	for (Patient patient : patientModel.findAll()) {
+
+	    Label firstName = new Label(patient.getFirstname());
+	    Label lastName = new Label(patient.getLastname());
+	    Label birthday = new Label(String.valueOf(patient.getBirthday()));
+
+	    Button detailsBtn = new Button("show details");
+	    detailsBtn.setData(patient);
+	    detailsBtn.addClickListener(event -> {
+		Patient pat = (Patient) event.getButton().getData();
+		showDetailsWindow(pat);
+	    });
+	    detailsBtn.addStyleName("link");
+
+	    // Create the table row
+	    table.addItem(new Object[] {firstName, lastName, birthday, detailsBtn},
+			    patient.getId());
+	}
+
+	this.addComponent(table);
+
+	// button to add patient
+	Button addNewPatientBtn = new Button("add new patient");
+	addNewPatientBtn.addClickListener(event -> {
+	    entryDetailsWindow();
 	});
-
-	this.addComponents(name, button, personTable);
-	this.setMargin(true);
-	this.setSpacing(true);
+	this.addComponent(addNewPatientBtn);
 
 	// last row
 	HorizontalLayout horizontalPages = new HorizontalLayout();
+	Button butTermine = new Button("Termine");
 	horizontalPages.addComponent(butTermine);
 	horizontalPages.setComponentAlignment(butTermine, Alignment.MIDDLE_LEFT);
+
+	Button butRoute = new Button("Route");
 	horizontalPages.addComponent(butRoute);
 	horizontalPages.setComponentAlignment(butRoute, Alignment.MIDDLE_CENTER);
+
+	Button butKalender = new Button("Kalender");
 	horizontalPages.addComponent(butKalender);
 	horizontalPages.setComponentAlignment(butKalender, Alignment.MIDDLE_RIGHT);
 	horizontalPages.setMargin(true);
-	butTermine.addStyleName("btntestclass");
-	butRoute.addStyleName("btntestclass");
-	butKalender.addStyleName("btntestclass");
+//	butTermine.addStyleName("btntestclass");
+//	butRoute.addStyleName("btntestclass");
+//	butKalender.addStyleName("btntestclass");
 	horizontalPages.setWidth("100%");
 	this.addComponent(horizontalPages);
 	panel.setContent(this);
+    }
+
+    // help method to connect to the PatientView class
+    private void showDetailsWindow(Patient patient) {
+	PatientView showPatient = new PatientView(patient);
+	final Window window = new Window();
+	window.setSizeFull();
+	window.center();
+	window.setContent(showPatient);
+	UI.getCurrent().addWindow(window);
+    }
+
+    // help method to entry a new patient data
+    public void entryDetailsWindow() {
+	Patient patEdit = new Patient();
+	final FormLayout formLayout = new FormLayout();
+
+	TextField firstNameEntry = new TextField("first name");
+	//	firstNameEntry.setRequired(true);
+	//	firstNameEntry.addValidator(new NullValidator("Must enter the first name!", false));
+	formLayout.addComponent(firstNameEntry);
+
+	TextField lastNameEntry = new TextField("last name");
+	//	lastNameEntry.setRequired(true);
+	//	lastNameEntry.addValidator(new NullValidator("Must enter the last name!", false));
+	formLayout.addComponent(lastNameEntry);
+
+	PopupDateField birthdayFill = new PopupDateField();
+	birthdayFill.setDateFormat("yyyy-MM-dd");
+	formLayout.addComponent(birthdayFill);
+
+	Button saveButn = new Button("save");
+	saveButn.addClickListener(event -> {
+	    PatientModel patMod = new PatientModel();
+	    patEdit.setFirstname(firstNameEntry.getValue());
+	    patEdit.setLastname(lastNameEntry.getValue());
+	    patEdit.setBirthday(birthdayFill.getValue());
+	    patMod.saveOrUpdate(patEdit);
+	});
+	formLayout.addComponent(saveButn);
+
+	final Window window = new Window();
+	window.setWidth(1000.0f, Unit.PIXELS);
+	window.center();
+	formLayout.setMargin(true);
+	window.setContent(formLayout);
+	UI.getCurrent().addWindow(window);
     }
 
     // method for the HealthVisUI
--- src/main/java/ch/bfh/bti7081/s2016/blue/hv/view/PatientView.java
+++ src/main/java/ch/bfh/bti7081/s2016/blue/hv/view/PatientView.java
@@ -1,70 +1,91 @@
 package ch.bfh.bti7081.s2016.blue.hv.view;
 
+import ch.bfh.bti7081.s2016.blue.hv.entities.Patient;
+import ch.bfh.bti7081.s2016.blue.hv.model.PatientModel;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.server.ThemeResource;
-import com.vaadin.ui.AbsoluteLayout;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Image;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
-/**
- * Created by uck1 on 14.05.2016.
- */
+
 public class PatientView extends VerticalLayout implements View {
 
     private static final String NAME = "Patient"; // variable for HealthVisUI
 
-    private Button butMenu = new Button("Menu");
-    private Button butCall = new Button("Call");
+    public PatientView(Patient patient) {
 
-    private Image picture = new Image(null, new ThemeResource("icons/Guy.png"));
-    private Label firstName = new Label("First Name: ");
-    private Label lastName = new Label("Last Name: ");
-    private Label age = new Label("Age: ");
-
-    private Button butBesuche = new Button("Besuche");
-    private Button butAnmerkungen = new Button("Anmerkungen");
-    private Button butThree = new Button("Choice 3");
-    private Button butFour = new Button("Choice 4");
-
-    public PatientView() {
 	setSizeFull();
-	buildLayout();
-    }
-
-    private void buildLayout() {
 
 	// 1st row
 	HorizontalLayout firstLay = new HorizontalLayout();
+	Button butMenu = new Button("Menu");
 	firstLay.addComponent(butMenu);
 	firstLay.setComponentAlignment(butMenu, Alignment.MIDDLE_LEFT);
+	Button butCall = new Button("Call");
 	firstLay.addComponent(butCall);
 	firstLay.setComponentAlignment(butCall, Alignment.MIDDLE_RIGHT);
 	firstLay.setMargin(true);
-	butMenu.addStyleName("btntestclass");
-	butCall.addStyleName("btntestclass");
+//	butMenu.addStyleName("btntestclass");
+//	butCall.addStyleName("btntestclass");
 	firstLay.setWidth("100%");
+	firstLay.setHeight("30%");
 	this.addComponent(firstLay);
 
 	// 2nd row horizontal splitted
 	HorizontalSplitPanel hsplit = new HorizontalSplitPanel();
+	hsplit.addStyleName("invisibleSplitter");
 //	hsplit.setHeight(5, Unit.CM);
 	hsplit.setSplitPosition(40, Unit.PERCENTAGE);
-	// left side
+	/*
+	 left side
+	  */
 	AbsoluteLayout left = new AbsoluteLayout();
+	Image picture = new Image(null, new ThemeResource("icons/Guy.png"));  // for testing
 	left.addComponent(picture, "left: 30px; top: 20px;");
 	// left.setMargin(true);
 	hsplit.setFirstComponent(left);
-	// right side
+	/*
+	 right side
+	  */
 	AbsoluteLayout right = new AbsoluteLayout();
+
+	TextField firstName = new TextField();
+	firstName.setConvertedValue(patient.getFirstname());
 	right.addComponent(firstName, "left: 10px; top: 20px;");
-	right.addComponent(lastName, "left: 10px; top: 50px;");
-	right.addComponent(age, "left: 10px; top: 80px;");
+	TextField lastName = new TextField();
+	lastName.setConvertedValue(patient.getLastname());
+	right.addComponent(lastName, "left: 10px; top: 70px;");
+	PopupDateField birthday = new PopupDateField();
+	birthday.setDateFormat("yyyy-MM-dd");
+	birthday.setValue(patient.getBirthday());
+	right.addComponent(birthday, "left: 10px; top: 120px;");
+	TextField street = new TextField();
+	street.setConvertedValue(patient.getContact().getStreet());
+	right.addComponent(street, "left: 10px; top: 170px;");
+	TextField zip = new TextField();
+	zip.setConvertedValue(patient.getContact().getZip());
+	right.addComponent(zip, "left: 10px; top: 220px;");
+	TextField city = new TextField();
+	city.setConvertedValue(patient.getContact().getCity());
+	right.addComponent(city, "left: 10px; top: 270px;");
+	TextField phoneNumber = new TextField();
+	phoneNumber.setConvertedValue(patient.getContact().getPhoneNumber());
+	right.addComponent(phoneNumber, "left: 10px; top: 320px;");
+
+	Button savButn = new Button("save");
+	savButn.addClickListener(event -> {
+	    PatientModel patMod = new PatientModel();
+	    patient.setFirstname(firstName.getValue());
+	    patient.setLastname(lastName.getValue());
+	    patient.setBirthday(birthday.getValue());
+	    patient.getContact().setStreet(street.getValue());
+	    patient.getContact().setZip(zip.getValue());
+	    patient.getContact().setCity(city.getValue());
+	    patient.getContact().setPhoneNumber(phoneNumber.getValue());
+	    patMod.saveOrUpdate(patient);
+	});
+	right.addComponent(savButn, "left: 10px; top: 390px;");
+
 	hsplit.setSecondComponent(right);
 	this.addComponent(hsplit); // add both to the row of the vertical layout
 
@@ -72,21 +93,32 @@ public class PatientView extends VerticalLayout implements View {
 	HorizontalSplitPanel buttonLay = new HorizontalSplitPanel();
 	buttonLay.setSplitPosition(50, Unit.PERCENTAGE);
 
-	// left side
+	/*
+	 left side
+	  */
 	AbsoluteLayout buttonLeftLay = new AbsoluteLayout();
-	buttonLeftLay.addComponent(new Image(null, new ThemeResource("icons/thumbs-up-circle-120x120.png")),
+	Button butBesuche = new Button("Besuche");
+	buttonLeftLay.addComponent(new Image(null, new ThemeResource("icons/double-cutted-circle-120x120.png")),
 		"left:60; top: 50px;");
 	butBesuche.addClickListener(e -> {});
-	buttonLeftLay.addComponent(new Image(null, new ThemeResource("icons/thumbs-down-circle-120x120.png")),
+
+	Button butAnmerkungen = new Button("Anmerkungen");
+	buttonLeftLay.addComponent(new Image(null, new ThemeResource("icons/double-cutted-circle-120x120.png")),
 		"left:60; top: 190px;");
 	butAnmerkungen.addClickListener(e -> {});
 	buttonLay.setFirstComponent(buttonLeftLay);
-	// right side
+
+	/*
+	 right side
+	  */
 	AbsoluteLayout buttonRightLay = new AbsoluteLayout();
-	buttonRightLay.addComponent(new Image(null, new ThemeResource("icons/thumbs-up-circle-120x120.png")),
+	Button butThree = new Button("Choice 3");
+	buttonRightLay.addComponent(new Image(null, new ThemeResource("icons/double-cutted-circle-120x120.png")),
 		"right:60; top: 50px;");
 	butThree.addClickListener(e -> {});
-	buttonRightLay.addComponent(new Image(null, new ThemeResource("icons/thumbs-down-circle-120x120.png")),
+
+	Button butFour = new Button("Choice 4");
+	buttonRightLay.addComponent(new Image(null, new ThemeResource("icons/double-cutted-circle-120x120.png")),
 		"right:60; top: 190px;");
 	butFour.addClickListener(e -> {});
 	buttonLay.setSecondComponent(buttonRightLay);
